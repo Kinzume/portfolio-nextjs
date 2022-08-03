5ea96e7a204e994ba4e9eb183f6fed6ae8a0db3c
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-03 17:08:30 +0100

    Redesigned skills section

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,134 +1,45 @@
 import Image from "next/image";
+import { ImHtmlFive2, ImCss3 } from "react-icons/im";
+import { SiJavascript, SiReact, SiTailwindcss } from "react-icons/si";
+import { FaGit } from "react-icons/fa";
 import React from "react";
 
 const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+        <p className="text-xl tracking-widest uppercase text-[#93FF75]">
           Skills
         </p>
         <h2 className="py-4">What I Can Do</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/html.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
-              </div>
-            </div>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="p-6 bg-[#0F1E35] hover:scale-105 ease-in duration-100 grid grid-cols-2 gap-4 justify-center items-center">
+            <ImHtmlFive2 className="m-auto" size="4rem" />
+            <h3 className="flex flex-col items-center justify-center">HTML</h3>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/css.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
-              </div>
-            </div>
+          <div className="p-6 bg-[#0F1E35] hover:scale-105 ease-in duration-100 grid grid-cols-2 gap-4 justify-center items-center">
+            <ImCss3 className="m-auto" size="4rem" />
+            <h3 className="flex flex-col items-center justify-center">CSS</h3>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/javascript.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>JavaScript</h3>
-              </div>
-            </div>
+          <div className="p-6 bg-[#0F1E35] hover:scale-105 ease-in duration-100 grid grid-cols-2 gap-4 justify-center items-center">
+            <SiJavascript className="m-auto" size="3.5rem" />
+            <h3 className="flex flex-col items-center justify-center">
+              JavaScript
+            </h3>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/react.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>React</h3>
-              </div>
-            </div>
+          <div className="p-6 bg-[#0F1E35] hover:scale-105 ease-in duration-100 grid grid-cols-2 gap-4 justify-center items-center">
+            <SiReact className="m-auto" size="3.5rem" />
+            <h3 className="flex flex-col items-center justify-center">React</h3>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/tailwind.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind</h3>
-              </div>
-            </div>
+          <div className="p-6 bg-[#0F1E35] hover:scale-105 ease-in duration-100 grid grid-cols-2 gap-4 justify-center items-center">
+            <SiTailwindcss className="m-auto" size="3.5rem" />
+            <h3 className="flex flex-col items-center justify-center">
+              Tailwind
+            </h3>
           </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/firebase.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>FireBase</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/github1.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>GitHub</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src="/../public/assets/skills/shopify.png"
-                  width="64px"
-                  height="64px"
-                  alt="/"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Shopify</h3>
-              </div>
-            </div>
+          <div className="p-6 bg-[#0F1E35] hover:scale-105 ease-in duration-100 grid grid-cols-2 gap-4 justify-center items-center">
+            <FaGit className="m-auto" size="3rem" />
+            <h3 className="flex flex-col items-center justify-center">Git</h3>
           </div>
         </div>
       </div>

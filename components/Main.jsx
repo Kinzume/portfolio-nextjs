1ee233155e54fe2a08f032e7bdb3181d7669d9b6
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-03 16:29:05 +0100

    Redesigned main section

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,13 +1,11 @@
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { BsFillPersonLinesFill } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
+import { BsArrowRightShort } from "react-icons/bs";
 import React from "react";
 
 const Main = () => {
   return (
     <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center ">
+        <div className=" space-y-5">
           <h1 className="py-4">
             Hi I'm <span className="text-[#93FF75]">Leander</span>
           </h1>
@@ -18,20 +16,10 @@ const Main = () => {
             consectetur, pariatur distinctio ducimus maiores officia nesciunt
             delectus, facere perferendis error beatae.
           </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaLinkedinIn />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <FaGithub />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <AiOutlineMail />
-            </div>
-            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
-              <BsFillPersonLinesFill />
-            </div>
-          </div>
+          <button className="flex items-center m-auto gap-1">
+            Projects
+            <BsArrowRightShort />
+          </button>
         </div>
       </div>
     </div>

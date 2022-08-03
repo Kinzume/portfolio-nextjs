ab678129d2e7167d9e7a2ef47eef1eae804aa4a8
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-03 13:10:22 +0100

    Main.jsx - hero text changes

--- components/Main.jsx
+++ components/Main.jsx
@@ -8,9 +8,6 @@ const Main = () => {
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET'S BUILD SOMETING TOGETHER
-          </p>{" "}
           <h1 className="py-4 text-gray-700 ">
             Hi I'm <span className="text-[#5651e5]">Clint</span>
           </h1>

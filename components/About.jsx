9d67eda112f21dc0d9892ea2251644a6d3ffe4e8
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-03 16:39:00 +0100

    Redesigned about section

--- components/About.jsx
+++ components/About.jsx
@@ -5,32 +5,27 @@ const About = () => {
     <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
-          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+          <p className="uppercase text-xl tracking-widest text-[#93FF75]">
             About
           </p>
-          <h2 className="py-2 text-gray-600">Who I Am</h2>
-          <p className="py-2 text-gray-600">
+          <h2 className="py-2">Who I Am</h2>
+          <p className="py-2">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
             assumenda laudantium voluptatem saepe quasi delectus facilis dicta
             ratione iste reiciendis numquam, facere odio? Numquam incidunt natus
             minima cupiditate dignissimos corporis!
           </p>
-          <p className="py-2 text-gray-600">
+          <p className="py-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fugit
             sunt consectetur earum officiis necessitatibus mollitia magni quod
             laborum ducimus!
           </p>
-          <p className="py-2 text-gray-600 underline cursor-pointer">
-            Check out some of my latest projects
-          </p>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
-          <img
-            className="rounded-xl"
-            src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-            alt="/"
-          />
-        </div>
+        <img
+          className="w-full h-auto m-auto shadow-md shadow-black hover:scale-105 ease-in duration-100"
+          src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="/"
+        />
       </div>
     </div>
   );

7a044e07f6d41d759e3b01f4a8784dfb92426648
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-03 17:32:41 +0100

    Added box shadow

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -5,8 +5,12 @@ import React from "react";
 const ProjectItem = ({ title, backgroundImg, projectUrl }) => {
   return (
     <div>
-      <div className="relative flex items-center justify-center h-auto w-full group hover:bg-[#93FF75] ease-in duration-300">
-        <Image className="group-hover:opacity-10" src={backgroundImg} alt="/" />
+      <div className="relative flex items-center justify-center h-auto w-full group shadow-md shadow-black  hover:bg-[#93FF75] ease-in duration-300">
+        <Image
+          className="group-hover:opacity-10 "
+          src={backgroundImg}
+          alt="/"
+        />
         <div className="text-[#000C1F] hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
           <h3 className="text-2xl  tracking-wider text-center">{title}</h3>
           <p className="pb-4 pt-2">React JS</p>

462d18fc7ef9d5234c564678144be5cc78a2c764
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-10 16:46:13 +0100

    Adjust styling to desktop navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -56,21 +56,27 @@ const Navbar = () => {
         <div>
           <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
             <Link href="/#home">
-              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+              <li className="ml-10 text-sm uppercase cursor-pointer hover:border-b hover:border-b-[#93FF75] hover:text-[#93FF75]">
+                Home
+              </li>
             </Link>
             <Link href="/#about">
-              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+              <li className="ml-10 text-sm uppercase cursor-pointer hover:border-b hover:border-b-[#93FF75] hover:text-[#93FF75]">
+                About
+              </li>
             </Link>
             <Link href="/#skills">
-              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+              <li className="ml-10 text-sm uppercase cursor-pointer hover:border-b hover:border-b-[#93FF75] hover:text-[#93FF75]">
+                Skills
+              </li>
             </Link>
             <Link href="/#projects">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+              <li className="ml-10 text-sm uppercase cursor-pointer hover:border-b hover:border-b-[#93FF75] hover:text-[#93FF75]">
                 Projects
               </li>
             </Link>
             <Link href="/#contact">
-              <li className="ml-10 text-sm uppercase hover:border-b">
+              <li className="ml-10 text-sm uppercase cursor-pointer hover:border-b hover:border-b-[#93FF75] hover:text-[#93FF75]">
                 Contact
               </li>
             </Link>

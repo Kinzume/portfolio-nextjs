e33342127dea13f41d04332e57d41582d4ea03b3
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-08-10 17:58:21 +0100

    Update img

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
-      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+      <div className="max-w-[1240px] m-auto md:grid items-center grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracking-widest text-[#93FF75]">
             About
@@ -38,11 +38,17 @@ const About = () => {
             development.
           </p>
         </div>
-        <img
-          className="w-full h-auto m-auto shadow-md shadow-black hover:scale-105 ease-in duration-100"
-          src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="/"
-        />
+        <picture>
+          <source
+            srcSet="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            type="image/webp"
+          />
+          <img
+            className="w-full h-auto m-auto shadow-md shadow-black hover:scale-105 ease-in duration-100"
+            src="https://images.unsplash.com/photo-1547394765-185e1e68f34e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            alt="/"
+          />
+        </picture>
       </div>
     </div>
   );
